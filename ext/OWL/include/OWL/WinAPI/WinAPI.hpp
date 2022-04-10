4deaf1ff1b19edbf31b069fede33f5764d9dbdaa
beProsto <lavionperavion2@gmail.com>
2022-04-10 15:17:24 +0200

    OWL with GLAD test

--- ext/OWL/include/OWL/WinAPI/WinAPI.hpp
+++
@@ -1,9 +0,0 @@
-#ifndef _OWL_WINAPI_WINAPI_HPP_HEADER_FILE_GUARD
-#define _OWL_WINAPI_WINAPI_HPP_HEADER_FILE_GUARD
-
-/* This file will include WinAPI */
-#include <windows.h>
-#include <winuser.h>
-#include <xinput.h>
-
-#endif/*_OWL_WINAPI_WINAPI_HPP_HEADER_FILE_GUARD*/
